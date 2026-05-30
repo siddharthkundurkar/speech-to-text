96e0760e418e1b25b6b83b52bc68ac6d6927232c
Siddharth Kundurkar <siddharthkundurkar@gmail.com>
2026-05-30 21:40:25 +0530

    Day 12 Task

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,13 @@ dotenv.config();
 
 import app from "./app.js";
 
-import connectDB from "./config/db.js";
+import connectDB
+from "./config/db.js";
 
 
 // Connect Database
 connectDB();
 
 
-// Start Server
-app.listen(5000, () => {
-
-  console.log("Server running on port 5000");
-
-});
+// Export App
+export default app;
