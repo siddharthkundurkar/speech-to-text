6c916c57792a0233a22b3f6708e08906ce9d2ffa
Siddharth Kundurkar <siddharthkundurkar@gmail.com>
2026-05-30 19:59:17 +0530

    Day 11 Task

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,11 @@ import uploadRoutes from "./routes/uploadRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.use("/api", uploadRoutes);
